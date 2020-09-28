815e7f47b2cb94d043111a1415443db0eb8be3cf
ShuvoDas1 <67542645+ShuvoDas1@users.noreply.github.com>
2020-09-28 23:40:40 +0600

    pushed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser')
+const objectId = require('mongodb').ObjectId;
+
 const password = 'bjix8DVun8Y4ADE';
 
 
@@ -25,13 +27,37 @@ client.connect(err => {
       })
       
   })
+
+  app.get('/product/:id',(req,res)=>{
+    productCollection.find({_id:objectId(req.params.id)})
+    .toArray((err,documents)=>{
+      res.send(documents[0]);
+    })
+  })
   
   app.post('/addProduct',(req,res) =>{
     const product = req.body;
       productCollection.insertOne(product)
       .then(result =>{
-        console.log('prodcut added successfully');
-        res.send('success')
+        console.log('product added successfully');
+        res.redirect('/')
+      })
+  })
+
+  app.patch('/update/:id',(req,res) =>{
+    productCollection.updateOne({_id:objectId(req.params.id)},
+    {
+      $set: {price : req.body.price, quantity: req.body.quantity}
+    })
+    .then(result =>{
+      res.send(result.modifiedCount > 0)
+    })
+  })
+
+  app.delete('/delete/:id',(req,res) => {
+      productCollection.deleteOne({_id:objectId(req.params.id)})  
+      .then(result => {
+        res.send(result.deletedCount > 0)
       })
   })
   
